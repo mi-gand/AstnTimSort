614317a5139f8ae63ccf5c8b8d3c718e3b48c5c7
VVSenti <sentyurinvv@gmail.com>
2024-10-23 00:51:38 +0300

    feature: validation for person class added. Person and PersonParser classes edited

--- src/main/java/com/aston/AstnTimSort/models/Person.java
+++ src/main/java/com/aston/AstnTimSort/models/Person.java
@@ -1,16 +1,12 @@
 package com.aston.AstnTimSort.models;
 
-import java.security.InvalidParameterException;
-
-import ch.qos.logback.core.util.StringUtil;
-
 public class Person implements Comparable<Person> {
 
 	private final String lastName;
 	private final Integer age;
 	private final GenderEnum gender;
 
-	public Person(String lastName, Integer age, GenderEnum gender) {
+	private Person(String lastName, Integer age, GenderEnum gender) {
 		this.lastName = lastName;
 		this.age = age;
 		this.gender = gender;
@@ -47,26 +43,37 @@ public class Person implements Comparable<Person> {
 		return "Person [lastName=" + lastName + ", age=" + age + ", gender=" + gender + "]";
 	}
 
-	static Builder getBuilder() {
+	public static Builder getBuilder() {
 		return new Builder();
 	}
 
-	static class Builder {
-		public Person build(String fieldValues) {
-			try {
-				String[] substrings = fieldValues.trim().split("\\s+");
-				int age = Integer.parseInt(substrings[1]);
-				GenderEnum gender = GenderEnum
-						.valueOf(StringUtil.capitalizeFirstLetter(substrings[2].toLowerCase()));
-				return new Person(substrings[0], age, gender);
-			} catch (Exception e) {
-				throw new InvalidParameterException();
-			}
-		}
-	}
+	public static class Builder {
+		private String lastName;
+		private Integer age;
+		private GenderEnum gender;
 
-	public static void main(String[] args) {
-		System.out.println(GenderEnum.valueOf("Male".toUpperCase()));
+		public Builder setLastName(String lastName) {
+			if (!lastName.chars().allMatch(Character::isLetter))
+				throw new IllegalArgumentException();
+			this.lastName = lastName;
+			return this;
+		}
+
+		public Builder setAge(Integer age) {
+			if (age <= 0)
+				throw new IllegalArgumentException();
+			this.age = age;
+			return this;
+		}
+
+		public Builder setGender(GenderEnum gender) {
+			this.gender = gender;
+			return this;
+		}
+
+		public Person build() {
+			return new Person(lastName, age, gender);
+		}
 	}
 
 }
--- src/main/java/com/aston/AstnTimSort/parsers/PersonParser.java
+++ src/main/java/com/aston/AstnTimSort/parsers/PersonParser.java
@@ -1,30 +1,48 @@
 package com.aston.AstnTimSort.parsers;
 
-import java.security.InvalidParameterException;
-
 import com.aston.AstnTimSort.models.Person;
 import com.aston.AstnTimSort.models.Person.GenderEnum;
 
-
 public class PersonParser implements StringParserToComparable<Person> {
-	
+
 	private final String PATTERN = "<Last name> <age> <male/female>";
+	private final String EXAMPLE = "Ivanov 35 male";
 
 	@Override
 	public Comparable<Person> parse(String input) {
-		try {
 		String[] substrings = input.trim().split("\\s+");
-		int age = Integer.parseInt(substrings[1]);
-		GenderEnum gender = GenderEnum.valueOf(substrings[2].toUpperCase());
-		return new Person(substrings[0], age, gender);
-		} catch (Exception e) {
-			throw new InvalidParameterException();
+		if (substrings.length != 3)
+			throw new IllegalArgumentException("There must be 3 parameters");
+		Person.Builder builder = Person.getBuilder();
+		String lastName = substrings[0];
+		try {
+			builder.setLastName(lastName);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Last name format is incorrect");
 		}
+		try {
+			builder.setAge(Integer.parseInt(substrings[1]));
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("Age format is incorrect");
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Age must be greater then zero");
+		}
+		try {
+			builder.setGender(GenderEnum.valueOf(substrings[2].toUpperCase()));
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Gender format is incorrect");
+		}
+		return builder.build();
 	}
 
 	@Override
 	public String getPattern() {
 		return PATTERN;
+	}
+
+	@Override
+	public String getInputExample() {
+		return EXAMPLE;
 	}
 
 }
